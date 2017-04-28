46214a4110e8df12b51f3d6233439b1798b21b1d
Elliot Wright <wright.elliot@gmail.com>
2017-04-28 18:13:25 +0100

    Playing around with concurrency, max things active at once.

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -3,10 +3,15 @@ package archive
 import (
 	"fmt"
 	"path"
+	"runtime"
+	"sort"
 	"time"
 )
 
-// ArchiveDirs takes an array of directory paths as strings, and a formatting string for the file
+// cores is the number of logical CPU cores the Go runtime has available to it.
+var cores = runtime.GOMAXPROCS(0)
+
+// Dirsf takes an array of directory paths as strings, and a formatting string for the file
 // names, and produces .tar.gz archives for each of the given directories. If any of the directories
 // don't exist, an error will be returned.
 //
@@ -24,6 +29,33 @@ func Dirsf(dirnames []string, namefmt string) ([]string, error) {
 	// Create the names for each archive that will be produced, based on the format provided.
 	namefmt = fmt.Sprintf("%s.tar.gz", namefmt)
 
+	limiter := make(chan bool, cores)
+
+	// Set up the limiter, we fill it with the number of cores we have available.
+	for i := 0; i < cores; i++ {
+		limiter <- true
+	}
+
+	for _, dirname := range dirnames {
+		<-limiter
+
+		go func(d string) {
+			// Simulate longer process.
+			time.Sleep(2 * time.Second)
+			fmt.Printf("%s (%d)\n", d, time.Now().Unix())
+
+			// Release "worker" availability
+			limiter <- true
+		}(dirname)
+	}
+
+	// Wait for all workers to finish.
+	for i := 0; i < cores; i++ {
+		<-limiter
+	}
+
+	// ...
+
 	// We probably want to batch this process, we should have that code lying around somewhere still
 	// with channels and threads and whatnot...
 	for _, dirname := range dirnames {
@@ -32,15 +64,21 @@ func Dirsf(dirnames []string, namefmt string) ([]string, error) {
 		tarFiles = append(tarFiles, fmt.Sprintf(namefmt, basename, timestamp))
 	}
 
+	fmt.Println(cores)
+
+	// Sort tar filenames so they still come out in alphabetical order.
+	sort.Strings(tarFiles)
+
 	return tarFiles, nil
 }
 
-// @note: Get CPU core count usable by Go from `runtime.GOMAXPROCS(0)`.
-//
 // @todo: Instead of splitting the data into chunks, can we process one dir at a time up to a limit
 // (the limit being the number of cores available to the Go runtime).
 //   This also has the benefit that if one core is faster than others for some reason, it won't sit
 //   idle while others have still handling their "chunk".
+//
+//   Can you count the length of a channel? If so, we should add to the channel of ongoing tasks
+//   before we start the Go routine for it, otherwise we could have race conditions.
 //
 // @todo: Can this be a pipeline'd process using channels?
 //   Maybe not, given it's going to be doing IO, we can use channels to inform the application about
