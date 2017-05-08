eaaac96494185803d7e7d04c057d8ccd3cdb7cc5
Elliot Wright <wright.elliot@gmail.com>
2017-05-08 17:47:23 +0100

    Added some missing comments and the test file for gcs.

--- pkg/storage/gcs.go
+++ pkg/storage/gcs.go
@@ -5,20 +5,44 @@ import (
 	"io"
 
 	gstorage "cloud.google.com/go/storage"
+	xcontext "golang.org/x/net/context"
 	"google.golang.org/api/option"
 )
 
-var gcsNewClient = gstorage.NewClient // For testing
+// gcsBucketRef is an interface that allows us to mock a Google Cloud Storage bucket handle.
+type gcsBucketRef interface {
+	Attrs(ctx xcontext.Context) (*gstorage.BucketAttrs, error)
+	Object(name string) *gstorage.ObjectHandle
+}
 
+// gcsClientRef is an interface that allows us to mock a Google Cloud Storage client.
+type gcsClientRef interface {
+	io.Closer
+
+	Bucket(name string) *gstorage.BucketHandle
+}
+
+var newGcsClientRefFn = gstorage.NewClient // For testing
+
+// newGcsClientRef is used to force the use of the new context package, and our interface upon the
+// Google Cloud Storage client library. It'd be nice if they provided some interfaces of their own!
+func newGcsClientRef(ctx context.Context, opts ...option.ClientOption) (gcsClientRef, error) {
+	return newGcsClientRefFn(ctx, opts...)
+}
+
+// GCSGateway is a gateway for interacting with Google Cloud Storage.
 type GCSGateway struct {
 	bucketName string
-	bucketRef  *gstorage.BucketHandle
-	clientRef  *gstorage.Client
+	bucketRef  gcsBucketRef
+	clientRef  gcsClientRef
 	options    []option.ClientOption
 }
 
+// NewGCSGateway sets up a new Google Cloud Storage gateway, including acquiring a client reference
+// and a bucket reference. If the client fails to set up, or the bucket can't be found then this
+// function will return an error.
 func NewGCSGateway(ctx context.Context, bktName string, opts []option.ClientOption) (Gateway, error) {
-	client, err := gcsNewClient(ctx, opts...)
+	client, err := newGcsClientRef(ctx, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -40,8 +64,10 @@ func NewGCSGateway(ctx context.Context, bktName string, opts []option.ClientOpti
 	return gateway, nil
 }
 
-func (g *GCSGateway) Store(ctx context.Context, filename string, in io.Reader) error {
-	object := g.bucketRef.Object(filename)
+// Store takes a name (used as the object name) and a reader (to pull data from) and writes the data
+// into the storage. We use `io.Copy` to safely handle large files.
+func (g *GCSGateway) Store(ctx context.Context, name string, in io.Reader) error {
+	object := g.bucketRef.Object(name)
 
 	out := object.NewWriter(ctx)
 
@@ -59,6 +85,7 @@ func (g *GCSGateway) Store(ctx context.Context, filename string, in io.Reader) e
 	return nil
 }
 
+// Close closes the underlying Google Cloud Storage client.
 func (g *GCSGateway) Close() error {
 	return g.clientRef.Close()
 }
