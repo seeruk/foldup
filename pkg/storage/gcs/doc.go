787983087dcf5731eb47fea22bb5e6c2c132b99c
Elliot Wright <wright.elliot@gmail.com>
2017-05-10 08:29:08 +0100

    Removed some unnecessary parts of package comment.

--- pkg/storage/gcs/doc.go
+++ pkg/storage/gcs/doc.go
@@ -1,7 +1,6 @@
 // Package gcs provides interfaces and wraps calls for interacting with Google Cloud Storage. This
 // package is mainly necessary because of the lack of interfaces provided by Google with their
-// client library, resulting in ballooning code structures that are nowhere near as easy to follow
-// (but actually might result in a simpler, more focused API where they're used).
+// client library.
 //
 // The package aims to be unit-testable without calling any external services, making storage
 // gateways easier to test.
